970e4394285bf3577f2410a9c03072b4017a52e4
Paul Gallagher <gallagher.paul@gmail.com>
2010-09-18 23:54:43 +0800

    add base postgres db writer test

--- test/integration/postgres_db_writer_base_test.rb
+++ test/integration/postgres_db_writer_base_test.rb
@@ -1,21 +1,29 @@
 require 'test_helper'
 
-require 'mysql2psql'
+require 'mysql2psql/postgres_db_writer'
 
 class PostgresDbWriterBaseTest < Test::Unit::TestCase
   attr_accessor :options
   class << self
     def startup
       seed_test_database
+      @@options = get_test_config( 'config_localmysql_to_db_convert_nothing.yml' )
     end
     def shutdown
     end
   end
   def setup
-
+  end
+  def teardown
+  end
+  def options
+    @@options
   end
   
   def test_pg_connection
+    assert_nothing_raised do
+      reader = Mysql2psql::PostgresDbWriter.new(options)
+    end
   end
 
 end
