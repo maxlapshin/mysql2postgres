2473dfed0aa15ec60775eef463592c99eb485db0
Paul Gallagher <gallagher.paul@gmail.com>
2010-09-18 14:35:18 +0800

    add basic mysqlreader integration tests

--- test/lib/test_helper.rb
+++ test/lib/test_helper.rb
@@ -9,4 +9,13 @@ end
 
 require 'ext_test_unit'
 
+def get_test_reader(configfile)
+  options = Mysql2psql::ConfigBase.new( "#{File.dirname(__FILE__)}/../fixtures/#{configfile}" )
+  seed_data = "#{File.dirname(__FILE__)}/../fixtures/seed_integration_tests.sql"
+  rc=system("mysql -u#{options.mysqlusername} #{options.mysqldatabase} < #{seed_data}")
+  raise StandardError unless rc
+  Mysql2psql::MysqlReader.new(options)
+rescue
+  raise StandardError.new("Failed to initialize integration test db. See README for setup requirements.")  
+end
 
