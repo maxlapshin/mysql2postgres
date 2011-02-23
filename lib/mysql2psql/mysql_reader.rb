038d1b2968b2c656cf9634e99834ce81934c3eb7
Nathan Esquenazi <nesquena@gmail.com>
2011-02-23 00:53:19 -0800

    Adds a reconnect to mysql in case it goes away!

--- lib/mysql2psql/mysql_reader.rb
+++ lib/mysql2psql/mysql_reader.rb
@@ -5,26 +5,26 @@ class Mysql2psql
   class MysqlReader
     class Field
     end
-  
+
     class Table
       attr_reader :name
-    
+
       def initialize(reader, name)
         @reader = reader
         @name = name
       end
-    
+
       @@types = %w(tiny enum decimal short long float double null timestamp longlong int24 date time datetime year set blob string var_string char).inject({}) do |list, type|
         list[eval("::Mysql::Field::TYPE_#{type.upcase}")] = type
         list
       end
-    
+
       @@types[246] = "decimal"
-    
+
       def columns
         @columns ||= load_columns
       end
-    
+
       def convert_type(type)
         case type
         when /int.* unsigned/
@@ -49,15 +49,15 @@ class Mysql2psql
            "double precision"
         else
           type
-        end 
+        end
       end
-    
+
       def load_columns
         @reader.reconnect
         result = @reader.mysql.list_fields(name)
         mysql_flags = ::Mysql::Field.constants.select {|c| c =~ /FLAG/}
         fields = []
-        @reader.mysql.query("EXPLAIN `#{name}`") do |res|
+        @reader.query("EXPLAIN `#{name}`") do |res|
           while field = res.fetch_row do
             length = field[1][/\((\d+)\)/, 1] if field[1] =~ /\((\d+)\)/
             length = field[1][/\((\d+),(\d+)\)/, 1] if field[1] =~ /\((\d+),(\d+)\)/
@@ -75,31 +75,31 @@ class Mysql2psql
             fields << desc
           end
         end
- 
+
         fields.select {|field| field[:auto_increment]}.each do |field|
-          @reader.mysql.query("SELECT max(`#{field[:name]}`) FROM `#{name}`") do |res|
+          @reader.query("SELECT max(`#{field[:name]}`) FROM `#{name}`") do |res|
             field[:maxval] = res.fetch_row[0].to_i
           end
         end
         fields
       end
-    
-    
+
+
       def indexes
         load_indexes unless @indexes
-        @indexes 
+        @indexes
       end
- 
+
       def foreign_keys
         load_indexes unless @foreign_keys
         @foreign_keys
       end
-    
+
       def load_indexes
         @indexes = []
         @foreign_keys = []
-      
-        @reader.mysql.query("SHOW CREATE TABLE `#{name}`") do |result|
+
+        @reader.query("SHOW CREATE TABLE `#{name}`") do |result|
           explain = result.fetch_row[1]
           explain.split(/\n/).each do |line|
             next unless line =~ / KEY /
@@ -123,55 +123,67 @@ class Mysql2psql
           end
         end
       end
-    
+
       def count_rows
-        @reader.mysql.query("SELECT COUNT(*) FROM `#{name}`")  do |res|
+        @reader.query("SELECT COUNT(*) FROM `#{name}`")  do |res|
           return res.fetch_row[0].to_i
         end
       end
-    
+
       def has_id?
-        !!columns.find {|col| col[:name] == "id"} 
+        !!columns.find {|col| col[:name] == "id"}
       end
-    
+
       def count_for_pager
         query = has_id? ? 'MAX(id)' : 'COUNT(*)'
-        @reader.mysql.query("SELECT #{query} FROM `#{name}`") do |res|
+        @reader.query("SELECT #{query} FROM `#{name}`") do |res|
           return res.fetch_row[0].to_i
         end
       end
- 
+
       def query_for_pager
         query = has_id? ? 'WHERE id >= ? AND id < ?' : 'LIMIT ?,?'
         "SELECT #{columns.map{|c| "`"+c[:name]+"`"}.join(", ")} FROM `#{name}` #{query}"
       end
     end
-  
+
     def connect
       @mysql = ::Mysql.connect(@host, @user, @passwd, @db, @port, @sock, @flag)
       @mysql.query("SET NAMES utf8")
       @mysql.query("SET SESSION query_cache_type = OFF")
     end
-  
+
     def reconnect
       @mysql.close rescue false
       connect
     end
-  
+
+    def query(*args)
+      @mysql.query(*args)
+    rescue Mysql::Error => e
+      if e.message =~ /gone away/i
+        self.reconnect
+        retry
+      else
+        puts "MySQL Query failed '#{args.inspect}' #{e.inspect}, #{e.inspect}"
+        puts e.backtrace[0,3].join("\n")
+      end
+    end
+
     def initialize(options)
-      @host, @user, @passwd, @db, @port, @sock, @flag = 
-        options.mysqlhostname('localhost'), options.mysqlusername, 
-        options.mysqlpassword, options.mysqldatabase, 
+      @host, @user, @passwd, @db, @port, @sock, @flag =
+        options.mysqlhostname('localhost'), options.mysqlusername,
+        options.mysqlpassword, options.mysqldatabase,
         options.mysqlport, options.mysqlsocket
       connect
     end
-  
+
     attr_reader :mysql
-  
+
     def tables
       @tables ||= @mysql.list_tables.map {|table| Table.new(self, table)}
     end
-  
+
     def paginated_read(table, page_size)
       count = table.count_for_pager
       return if count < 1
