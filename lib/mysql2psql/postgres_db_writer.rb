aa03a411350fcd24e3cffefe1c11dcf9bf757468
Nathan Esquenazi <nesquena@gmail.com>
2011-03-02 03:25:35 -0800

    Adds robust retry support for postgres db writer

--- lib/mysql2psql/postgres_db_writer.rb
+++ lib/mysql2psql/postgres_db_writer.rb
@@ -4,26 +4,64 @@ require 'mysql2psql/postgres_writer'
 
 class Mysql2psql
 
+class SafePgConnector
+  def initialize(*args)
+    @conn = PGconn.new(*args)
+    self.open
+  end
+
+  def open
+    @conn.exec("SET search_path TO #{PGconn.quote_ident(schema)}") if schema
+    @conn.exec("SET client_encoding = 'UTF8'")
+    @conn.exec("SET standard_conforming_strings = off") if @conn.server_version >= 80200
+    @conn.exec("SET check_function_bodies = false")
+    @conn.exec("SET client_min_messages = warning")
+  end
+
+  def exec(*args)
+    @conn.exec(*args)
+  rescue Exception => e
+    $stderr.puts "Mysql2psql: Pg failed: #{e.to_s}, retrying"
+    $stderr.puts e
+    $stderr.puts e.backtrace[0,3].join("\n")
+    @conn.close
+    sleep(1)
+    self.open
+    retry
+  end
+
+  def method_missing(name, *args, &block)
+    if @conn.respond_to?(name)
+      @conn.send(name, *args, &block)
+    else
+      super
+    end
+  end
+
+  def respond_to?(name)
+    if @conn.respond_to?(name)
+     true
+    else
+      super
+    end
+  end
+end
+
 class PostgresDbWriter < PostgresWriter
   attr_reader :conn, :hostname, :login, :password, :database, :schema, :port
-  
+
   def initialize(options)
-    @hostname, @login, @password, @database, @port =  
-      options.pghostname('localhost'), options.pgusername, 
+    @hostname, @login, @password, @database, @port =
+      options.pghostname('localhost'), options.pgusername,
       options.pgpassword, options.pgdatabase, options.pgport(5432).to_s
     @database, @schema = database.split(":")
     open
   end
 
   def open
-    @conn = PGconn.new(hostname, port, '', '', database, login, password)
-    @conn.exec("SET search_path TO #{PGconn.quote_ident(schema)}") if schema
-    @conn.exec("SET client_encoding = 'UTF8'")
-    @conn.exec("SET standard_conforming_strings = off") if @conn.server_version >= 80200
-    @conn.exec("SET check_function_bodies = false")
-    @conn.exec("SET client_min_messages = warning")    
+    @conn = SafePgConnector.new(hostname, port, '', '', database, login, password)
   end
-  
+
   def close
     @conn.close
   end
@@ -32,13 +70,13 @@ class PostgresDbWriter < PostgresWriter
     rc = @conn.exec("SELECT COUNT(*) FROM pg_class WHERE relname = '#{relname}'")
     (!rc.nil?) && (rc.to_a.length==1) && (rc.first.count.to_i==1)
   end
-  
+
   def write_table(table)
     puts "Creating table #{table.name}..."
     primary_keys = []
     serial_key = nil
     maxval = nil
-    
+
     columns = table.columns.map do |column|
       if column[:auto_increment]
         serial_key = column[:name]
@@ -49,7 +87,7 @@ class PostgresDbWriter < PostgresWriter
       end
       "  " + column_description(column)
     end.join(",\n")
-    
+
     if serial_key
       if @conn.server_version < 80200
         serial_key_seq = "#{table.name}_#{serial_key}_seq"
@@ -64,10 +102,10 @@ class PostgresDbWriter < PostgresWriter
         NO MINVALUE
         CACHE 1
       EOF
-    
+
       @conn.exec "SELECT pg_catalog.setval('#{table.name}_#{serial_key}_seq', #{maxval}, true)"
     end
-    
+
     if @conn.server_version < 80200
       @conn.exec "DROP TABLE #{PGconn.quote_ident(table.name)} CASCADE;" if exists?(table.name)
     else
@@ -81,15 +119,15 @@ class PostgresDbWriter < PostgresWriter
       raise
     end
     puts "Created table #{table.name}"
- 
+
   end
-  
+
   def write_indexes(table)
     puts "Indexing table #{table.name}..."
     if primary_index = table.indexes.find {|index| index[:primary]}
       @conn.exec("ALTER TABLE #{PGconn.quote_ident(table.name)} ADD CONSTRAINT \"#{table.name}_pkey\" PRIMARY KEY(#{primary_index[:columns].map {|col| PGconn.quote_ident(col)}.join(", ")})")
     end
-    
+
     table.indexes.each do |index|
       next if index[:primary]
       unique = index[:unique] ? "UNIQUE " : nil
@@ -108,8 +146,8 @@ class PostgresDbWriter < PostgresWriter
       end
       @conn.exec("CREATE #{unique}INDEX #{PGconn.quote_ident(indexname)} ON #{PGconn.quote_ident(table.name)} (#{index[:columns].map {|col| PGconn.quote_ident(col)}.join(", ")});")
     end
-    
-    
+
+
     #@conn.exec("VACUUM FULL ANALYZE #{PGconn.quote_ident(table.name)}")
     puts "Indexed table #{table.name}"
   rescue Exception => e
@@ -117,7 +155,7 @@ class PostgresDbWriter < PostgresWriter
     puts e
     puts e.backtrace[0,3].join("\n")
   end
-  
+
   def write_constraints(table)
     table.foreign_keys.each do |key|
       key_sql = "ALTER TABLE #{PGconn.quote_ident(table.name)} ADD FOREIGN KEY (#{PGconn.quote_ident(key[:column])}) REFERENCES #{PGconn.quote_ident(key[:ref_table])}(#{PGconn.quote_ident(key[:ref_column])})"
@@ -128,7 +166,7 @@ class PostgresDbWriter < PostgresWriter
       end
     end
   end
-  
+
   def format_eta (t)
     t = t.to_i
     sec = t % 60
@@ -136,7 +174,7 @@ class PostgresDbWriter < PostgresWriter
     hour = t / 3600
     sprintf("%02dh:%02dm:%02ds", hour, min, sec)
   end
-  
+
   def write_contents(table, reader)
     _time1 = Time.now
     copy_line = "COPY #{PGconn.quote_ident(table.name)} (#{table.columns.map {|column| PGconn.quote_ident(column[:name])}.join(", ")}) FROM stdin;"
@@ -151,7 +189,7 @@ class PostgresDbWriter < PostgresWriter
       line = []
       process_row(table, row)
       @conn.put_copy_data(row.join("\t") + "\n")
-       
+
       if counter != 0 && counter % 20000 == 0
         elapsedTime = Time.now - _time1
         eta = elapsedTime * rowcount / counter - elapsedTime
@@ -160,19 +198,19 @@ class PostgresDbWriter < PostgresWriter
         printf "\r#{counter} of #{rowcount} rows loaded. [ETA: #{etatimef} (#{etaf})]"
         STDOUT.flush
       end
-      
+
       if counter % 5000 == 0
         @conn.put_copy_end
         @conn.exec(copy_line)
       end
-       
+
     end
     _time2 = Time.now
     puts "\n#{_counter} rows loaded in #{((_time2 - _time1) / 60).round}min #{((_time2 - _time1) % 60).round}s"
 #    @conn.putline(".\n")
     @conn.put_copy_end
   end
-  
+
 end
 
 end
