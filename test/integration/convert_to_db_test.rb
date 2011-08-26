8c6a1feff6155c5f47c8ee68d290b33aabe0f0e0
Aaron Peckham <apeckham@local>
2011-08-26 14:58:11 -0700

    make tests pass by moving startup/shutdown into setup/teardown. fixes uninitialized class variable errors: https://gist.github.com/4c76c2894b51ac911fba

--- test/integration/convert_to_db_test.rb
+++ test/integration/convert_to_db_test.rb
@@ -4,28 +4,22 @@ require 'mysql2psql'
 
 class ConvertToDbTest < Test::Unit::TestCase
 
-  class << self
-    def startup
-      seed_test_database
-      @@options=get_test_config_by_label(:localmysql_to_db_convert_all)
-      @@mysql2psql = Mysql2psql.new([@@options.filepath])
-      @@mysql2psql.convert
-      @@mysql2psql.writer.open
-    end
-    def shutdown
-      @@mysql2psql.writer.close
-      delete_files_for_test_config(@@options)
-    end
-  end
   def setup
+    seed_test_database
+    @options=get_test_config_by_label(:localmysql_to_db_convert_all)
+    @mysql2psql = Mysql2psql.new([@options.filepath])
+    @mysql2psql.convert
+    @mysql2psql.writer.open
   end
   def teardown
+    @mysql2psql.writer.close
+    delete_files_for_test_config(@options)
   end
 
   def test_table_creation
-    assert_true @@mysql2psql.writer.exists?('numeric_types_basics')
-    assert_true @@mysql2psql.writer.exists?('basic_autoincrement')
-    assert_true @@mysql2psql.writer.exists?('numeric_type_floats')
+    assert_true @mysql2psql.writer.exists?('numeric_types_basics')
+    assert_true @mysql2psql.writer.exists?('basic_autoincrement')
+    assert_true @mysql2psql.writer.exists?('numeric_type_floats')
   end
 
 end
--- test/integration/mysql_reader_base_test.rb
+++ test/integration/mysql_reader_base_test.rb
@@ -4,31 +4,23 @@ require 'mysql2psql/mysql_reader'
 
 class MysqlReaderBaseTest < Test::Unit::TestCase
 
-  class << self
-    def startup
-      seed_test_database
-      @@options = get_test_config_by_label(:localmysql_to_file_convert_nothing)
-    end
-    def shutdown
-      delete_files_for_test_config(@@options)
-    end
-  end
   def setup
+    seed_test_database
+    @options = get_test_config_by_label(:localmysql_to_file_convert_nothing)
   end
+
   def teardown
-  end
-  def options
-    @@options
+    delete_files_for_test_config(@options)
   end
   
   def test_mysql_connection
     assert_nothing_raised do
-      reader = Mysql2psql::MysqlReader.new(options)
+      reader = Mysql2psql::MysqlReader.new(@options)
     end
   end
   def test_mysql_reconnect
     assert_nothing_raised do
-      reader = Mysql2psql::MysqlReader.new(options)
+      reader = Mysql2psql::MysqlReader.new(@options)
       reader.reconnect
     end
   end
