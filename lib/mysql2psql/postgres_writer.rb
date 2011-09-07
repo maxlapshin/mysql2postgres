3db085bf0e19f402959eee91843decd99275a62e
James Coleman <jtc331@gmail.com>
2011-09-07 14:46:18 -0400

    Fix indentation and remove a tab.

--- lib/mysql2psql/postgres_writer.rb
+++ lib/mysql2psql/postgres_writer.rb
@@ -8,11 +8,11 @@ class Mysql2psql
     def column_description(column)
       "#{PGconn.quote_ident(column[:name])} #{column_type_info(column)}"
     end
-  
+
     def column_type(column)
       column_type_info(column).split(" ").first
     end
-  
+
     def column_type_info(column)
       if column[:auto_increment]
         return "integer DEFAULT nextval('#{column[:table_name]}_#{column[:name]}_seq'::regclass) NOT NULL"
@@ -21,7 +21,7 @@ class Mysql2psql
       null = column[:null] ? "" : " NOT NULL"
       type = 
       case column[:type]
-    
+
       # String types
       when "char"
         default = default + "::char" if default
@@ -29,7 +29,7 @@ class Mysql2psql
       when "varchar"
         default = default + "::character varying" if default
         "character varying(#{column[:length]})"
-      
+
       # Integer and numeric types
       when "integer"
         default = " DEFAULT #{column[:default].nil? ? 'NULL' : column[:default].to_i}" if default
@@ -40,7 +40,7 @@ class Mysql2psql
       when /tinyint|smallint/
         default = " DEFAULT #{column[:default].nil? ? 'NULL' : column[:default].to_i}" if default
         "smallint"
-    
+
       when "boolean"
         default = " DEFAULT #{column[:default].to_i == 1 ? 'true' : 'false'}" if default
         "boolean"
@@ -104,35 +104,34 @@ class Mysql2psql
       end
       "#{type}#{default}#{null}"
     end
-  
-    def process_row(table, row)
-    	table.columns.each_with_index do |column, index|
 
-          if column[:type] == "time"
-            row[index] = "%02d:%02d:%02d" % [row[index].hour, row[index].minute, row[index].second]
-          end
-        
-          if row[index].is_a?(Mysql::Time)
-            row[index] = row[index].to_s.gsub('0000-00-00 00:00', '1970-01-01 00:00')
-            row[index] = row[index].to_s.gsub('0000-00-00 00:00:00', '1970-01-01 00:00:00')
-          end
-        
-          if column_type(column) == "boolean"
-            row[index] = row[index] == 1 ? 't' : row[index] == 0 ? 'f' : row[index]
-          end
-        
-          if row[index].is_a?(String)
-            if column_type(column) == "bytea"
-              row[index] = PGconn.escape_bytea(row[index])
-            else
-              row[index] = row[index].gsub(/\\/, '\\\\\\').gsub(/\n/,'\n').gsub(/\t/,'\t').gsub(/\r/,'\r').gsub(/\0/, '')
-            end
-          end
-        
-          row[index] = '\N' if !row[index]
+    def process_row(table, row)
+      table.columns.each_with_index do |column, index|
+        if column[:type] == "time"
+          row[index] = "%02d:%02d:%02d" % [row[index].hour, row[index].minute, row[index].second]
         end
+
+        if row[index].is_a?(Mysql::Time)
+          row[index] = row[index].to_s.gsub('0000-00-00 00:00', '1970-01-01 00:00')
+          row[index] = row[index].to_s.gsub('0000-00-00 00:00:00', '1970-01-01 00:00:00')
+        end
+
+        if column_type(column) == "boolean"
+          row[index] = row[index] == 1 ? 't' : row[index] == 0 ? 'f' : row[index]
+        end
+
+        if row[index].is_a?(String)
+          if column_type(column) == "bytea"
+            row[index] = PGconn.escape_bytea(row[index])
+          else
+            row[index] = row[index].gsub(/\\/, '\\\\\\').gsub(/\n/,'\n').gsub(/\t/,'\t').gsub(/\r/,'\r').gsub(/\0/, '')
+          end
+        end
+
+        row[index] = '\N' if !row[index]
+      end
     end
-  
+
     def truncate(table)
     end
 
