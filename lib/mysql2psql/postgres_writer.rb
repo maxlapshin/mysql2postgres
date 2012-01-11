bbd31d74599fcac7b922faf753e95e1118922ffd
Tim Morgan <tim@timmorgan.org>
2012-01-11 13:32:42 -0600

    Default to 5 digit precision.

--- lib/mysql2psql/postgres_writer.rb
+++ lib/mysql2psql/postgres_writer.rb
@@ -54,7 +54,7 @@ class Mysql2psql
         "real"
       when "decimal"
         default = " DEFAULT #{column[:default].nil? ? 'NULL' : column[:default]}" if default
-        "numeric(#{column[:length] || 10}, #{column[:decimals] || 0})"
+        "numeric(#{column[:length] || 10}, #{column[:decimals] || 5})"
 
       when "double precision"
         default = " DEFAULT #{column[:default].nil? ? 'NULL' : column[:default]}" if default
