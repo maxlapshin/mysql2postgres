90fb9d5f2958e1cf13a2619f98de07a3715c2e9a
Juga Paazmaya <paazmaya@yahoo.com>
2014-11-19 09:53:35 +0200

    Configuration file no longer passed to as a file name, but parsed earlier

--- test/units/config_base_test.rb
+++ test/units/config_base_test.rb
@@ -4,10 +4,10 @@ require 'mysqltopostgres'
 #
 #
 class ConfigBaseTest < Test::Unit::TestCase
-  attr_reader :config, :configfilepath
+  attr_reader :config, :yaml
   def setup
-    @configfilepath = "#{File.dirname(__FILE__)}/../fixtures/config_all_options.yml"
-    @config = Mysql2psql::ConfigBase.new(configfilepath)
+    @yaml = YAML.load_file "#{File.dirname(__FILE__)}/../fixtures/config_all_options.yml"
+    @config = Mysql2psql::ConfigBase.new(yaml)
   end
 
   def teardown
@@ -16,7 +16,7 @@ class ConfigBaseTest < Test::Unit::TestCase
 
   def test_config_loaded
     assert_not_nil config.config
-    assert_equal configfilepath, config.filepath
+    assert_equal yaml, config.config
   end
 
   def test_uninitialized_error_when_not_found_and_no_default
